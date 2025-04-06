f5862069f06a20095749584ae2430ab8e88cc65f
reri <reri0702@gmail.com>
2025-04-06 10:18:52 +0900

    - add > login sql function

--- server/server_sql.c
+++ server/server_sql.c
@@ -75,6 +75,60 @@ int join_user(const char *id, const char *passwd)
 
 int login_user(const char *id, const char *passwd)
 {
+    MYSQL_RES *result = NULL;
+    MYSQL_ROW row = NULL;
     
-    return 0;
+    char    query[256]  = {0,};
+    int     ret         = -1;
+
+    unsigned char   enc_passwd[EVP_MAX_MD_SIZE] = {0,};
+    char            *base64passwd               = NULL;
+
+    int encpwlen = strlen(passwd);
+
+    SHA256_encrypt((unsigned char *)passwd, encpwlen, enc_passwd);
+    
+    base64passwd = BASE64_encode(enc_passwd, (EVP_MAX_MD_SIZE/2));
+    if (base64passwd == NULL)
+    {
+        fprintf(stderr, "password encode failed. \n");
+        return ret;
+    }
+    
+    snprintf(query, sizeof(query), 
+                "SELECT PASSWORD FROM CLIENT_INFO WHERE USERNAME = '%s'",
+                id);
+        
+    if (mysql_query(conn, query))
+    {
+        fprintf(stderr, "query failed: %s\n", mysql_error(conn));
+        free(base64passwd);
+        return ret;
+    }
+
+    result = mysql_store_result(conn);
+    if (result == NULL)
+    {
+        fprintf(stderr, "mysql_store_result() failed: %s\n", mysql_error(conn));
+        free(base64passwd);
+        return ret;
+    }
+
+    row = mysql_fetch_row(result);
+    if (!row)
+    {
+        fprintf(stdout, "not exist user : %s \n", id);
+        free(base64passwd);
+        mysql_free_result(result);
+        return ret;
+    }
+
+    if ( strstr(base64passwd, row[0]) != NULL )
+        ret = 0;
+
+    mysql_free_result(result);
+    
+    free(base64passwd);
+    
+    return ret;
 }
